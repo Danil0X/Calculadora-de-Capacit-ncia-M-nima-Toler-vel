5afdfc1345873a117b89f21a2bec2c62bb1b1fa4
Danilo <danilo.antonio.da.silva.2004@gmail.com>
2025-02-14 20:02:49 -0300

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Este código tem como função ser uma ferramenta para auxiliar a saber se um capacitor, ou banco de capacitores apresenta capacitância dentro do limite mínimo padrão de 20%
+
+Com a função "adicionar capacitor" é possível somar os capacitores que estão em paralelo no circuito, não sendo necessário realizar o cálculo
---
+++ app.js
@@ -0,0 +1,42 @@
+exibirTexto ('h1', 'digite o valor do capacitor:');
+exibirTexto ('p', 'para saber qual a sua tolerância em 20%');
+let listaDeCapacitores = [];
+
+function calcular(){
+    let valorInserido = document.querySelector('input').value; 
+    listaDeCapacitores.push (Number (valorInserido));
+    let capacitanciaTotal = listaDeCapacitores.reduce((acumulador, valorAtual) => acumulador + valorAtual);
+    let toleranciaDoCapacitor = capacitanciaTotal - capacitanciaTotal * 0.2;
+
+    exibirTexto ('p', '');
+    exibirTexto ('h1', `o valor mínimo tolerável de um capacitor de ${capacitanciaTotal}uF é ${toleranciaDoCapacitor}uF`)
+
+    document.querySelector('input').value = '';
+    listaDeCapacitores = [];
+}
+
+function exibirTexto (tag,titulo){
+    let texto = document.querySelector(tag);
+    texto.innerHTML = titulo;
+}
+
+function recomecar (){
+    exibirTexto ('h1', 'digite o valor do capacitor:');
+    exibirTexto ('p', 'para saber qual a sua tolerância em 20%');    
+
+    valorInserido = document.querySelector('input');
+    valorInserido.value = ''
+
+    listaDeCapacitores = [];
+}
+
+function adicionarCapacitor (){
+    let valorInserido = document.querySelector('input').value; 
+    listaDeCapacitores.push (Number (valorInserido));
+    let capacitanciaTotal = listaDeCapacitores.reduce((acumulador, valorAtual) => acumulador + valorAtual);
+
+    exibirTexto ('h1', `Somando capacitores, insira o valor que deseja adicionar`);
+    exibirTexto ('p', `Valor atual: ${capacitanciaTotal}`);
+
+    document.querySelector('input').value = ''
+}   
